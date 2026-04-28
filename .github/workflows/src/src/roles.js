31a96ee5ccfd19d15c7e18687a76a6be98a6cd18
Vineeth Kodanda <112724763+Vineeth2696@users.noreply.github.com>
2026-04-28 18:43:31 -0500

    Create roles.js

---
+++ .github/workflows/src/src/roles.js
@@ -0,0 +1,37 @@
+const FRONTEND_ROLES = [
+  "Frontend Developer",
+  "Front-End Engineer",
+  "React Developer",
+  "React.js Developer",
+  "Angular Developer",
+  "Vue.js Developer",
+  "UI Developer",
+  "UI Engineer",
+  "TypeScript Developer",
+  "Next.js Developer",
+  "JavaScript Developer",
+  "Full Stack Developer",
+  "Full Stack Engineer",
+  "React Native Developer",
+  "Web Developer",
+  "UI/UX Developer",
+  "Software Engineer Frontend",
+  "Senior Frontend Developer",
+  "Senior React Developer",
+  "Senior Angular Developer",
+  "Lead Frontend Engineer",
+  "Staff Frontend Engineer",
+];
+
+const CANDIDATE_SKILLS = [
+  "Angular","React","TypeScript","JavaScript","NestJS",
+  "GraphQL","Node.js","NgRx","RxJS","Jest","Cypress",
+  "CI/CD","REST APIs","Java","HTML","CSS","Nx",
+];
+
+const HIGH_PRIORITY_KEYWORDS = [
+  "Angular","React","TypeScript","NestJS","GraphQL",
+  "Senior","Lead","Staff","fintech",
+];
+
+module.exports = { FRONTEND_ROLES, CANDIDATE_SKILLS, HIGH_PRIORITY_KEYWORDS };
